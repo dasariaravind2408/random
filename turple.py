a1295caa4c24a0c7023631c0550d47e6787e4604
dasariaravind2408 <dasariaravind2408@gmail.com>
2026-08-19 08:33:21 -0700

    init

---
+++ turple.py
@@ -0,0 +1,8 @@
+
+inventory = [("notebooks", 42),("pens", 130),("staplers", 8),]
+
+item, quantity = inventory[0]
+print(f"{item}: {quantity} in stock")
+
+item, quantity = inventory[1]
+print(f"{item}: {quantity} in stock")
